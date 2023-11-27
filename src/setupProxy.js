057e84d25eb53588366469159626a64ad160c252
GarrChri <garrchri@oregonstate.edu>
2023-11-26 19:58:11 -0800

    removed unused files

--- src/setupProxy.js
+++
@@ -1,3 +0,0 @@
-const { createProxyMiddleware } = require('http-proxy-middleware');
-
-module.exports = function(app) { app.use( '/api', createProxyMiddleware({ target: 'http://localhost:26262', changeOrigin: true, }) );};
